6a45f6e2122170099747e7999805d9692c116ba0
Andrey Pikelner <pikelner@theor.jinr.ru>
2014-12-16 16:07:12 +0300

    mfitter src missing

---
+++ mr/mfitter.hpp
@@ -0,0 +1,73 @@
+//
+// MR - 2-loop matching and 3-loop Running, including full 2-loop EW corrections
+// Copyright (C) 2014 Andrey Pikelner <pikelner@theor.jinr.ru>
+//
+// This file is part of MR.
+//
+// MR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MR.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __MFITTER_HPP__
+#define __MFITTER_HPP__ 
+
+#include <vector>
+#include "sminput.hpp"
+#include "Minuit2/FCNBase.h"
+
+
+std::vector<long double> observables(long double,
+                                     long double,
+                                     long double,
+                                     long double,
+                                     long double,
+                                     long double,
+                                     long double);
+
+
+class MfitterFcn : public ROOT::Minuit2::FCNBase {
+  
+public:
+  
+  MfitterFcn(const std::vector<double>& meas,
+             // const std::vector<double>& pos,
+             const std::vector<double>& mvar,
+             double scale) : theMeasurements(meas),
+                             // thePositions(pos),
+                             theMVariances(mvar),
+                             theScale(scale),
+                             theErrorDef(1.) {}
+
+  ~MfitterFcn() {}
+
+  virtual double Up() const {return theErrorDef;}
+  virtual double operator()(const std::vector<double>&) const;
+  
+  std::vector<double> measurements() const {return theMeasurements;}
+  // std::vector<double> positions() const {return thePositions;}
+  std::vector<double> variances() const {return theMVariances;}
+
+  void setErrorDef(double def) {theErrorDef = def;}
+
+private:
+
+  
+  std::vector<double> theMeasurements;
+  // std::vector<double> thePositions;
+  std::vector<double> theMVariances;
+  double theErrorDef;
+  double theScale;
+};
+
+
+#endif  // __MFITTER_HPP__ 
---
+++ mr/mttos01.cpp
@@ -0,0 +1,16 @@
+#include <tt.hpp>
+long double tt<MS>::x01(size_t nL, size_t nH, size_t boson)
+{     
+      
+      
+    std::complex<long double> armttos[5], mttosret;
+
+    armttos[1]=double(boson);
+    armttos[2]=Tsil::A(mmt,mu2);
+    armttos[3]=pow(mmt,-1);
+   armttos[4]= - armttos[3]*armttos[2];
+   armttos[4]=1./3. + armttos[4];
+
+      mttosret = 4*armttos[4]*armttos[1];
+      return mttosret.real();
+}
