114782a1f584c9719ff27c94137d7cf7f6fbec7f
Alexander Bednyakov <bednya@theor.jinr.ru>
2015-01-07 16:21:32 +0300

    double -> long double in math module

--- math/mrimpl.cpp
+++ math/mrimpl.cpp
@@ -6,8 +6,58 @@
 #include "ZZ.hpp"
 #include "HH.hpp"
 #include "tt.hpp"
+#include "dr.hpp"
 
 
+const WW<MS> & get_WWbar(const MSinput& mi, long double mu2)
+{
+  static std::map< std::pair<MSinput, long double>, WW<MS> > directory;
+  std::map< std::pair<MSinput, long double>, WW<MS> >::iterator i = directory.find(std::make_pair(mi,mu2));
+  if (i != directory.end())
+    return i->second;
+  else
+    return directory.insert(std::make_pair(std::make_pair(mi,mu2), WW<MS>(mi,mu2))).first->second;
+}
+
+const ZZ<MS> & get_ZZbar(const MSinput& mi, long double mu2)
+{
+  static std::map< std::pair<MSinput, long double>, ZZ<MS> > directory;
+  std::map< std::pair<MSinput, long double>, ZZ<MS> >::iterator i = directory.find(std::make_pair(mi,mu2));
+  if (i != directory.end())
+    return i->second;
+  else
+    return directory.insert(std::make_pair(std::make_pair(mi,mu2), ZZ<MS>(mi,mu2))).first->second;
+}
+
+const HH<MS> & get_HHbar(const MSinput& mi, long double mu2)
+{
+  static std::map< std::pair<MSinput, long double>, HH<MS> > directory;
+  std::map< std::pair<MSinput, long double>, HH<MS> >::iterator i = directory.find(std::make_pair(mi,mu2));
+  if (i != directory.end())
+    return i->second;
+  else
+    return directory.insert(std::make_pair(std::make_pair(mi,mu2), HH<MS>(mi,mu2))).first->second;
+}
+
+const tt<MS> & get_ttbar(const MSinput& mi, long double mu2)
+{
+  static std::map< std::pair<MSinput, long double>, tt<MS> > directory;
+  std::map< std::pair<MSinput, long double>, tt<MS> >::iterator i = directory.find(std::make_pair(mi,mu2));
+  if (i != directory.end())
+    return i->second;
+  else
+    return directory.insert(std::make_pair(std::make_pair(mi,mu2), tt<MS>(mi,mu2))).first->second;
+}
+
+const dr<MS> & get_drbar(const MSinput& mi, long double mu2)
+{
+  static std::map< std::pair<MSinput, long double>, dr<MS> > directory;
+  std::map< std::pair<MSinput, long double>, dr<MS> >::iterator i = directory.find(std::make_pair(mi,mu2));
+  if (i != directory.end())
+    return i->second;
+  else
+    return directory.insert(std::make_pair(std::make_pair(mi,mu2), dr<MS>(mi,mu2))).first->second;
+}
 
 const WW<OS> & get_WW(const OSinput& oi, long double mu2)
 {
@@ -39,14 +89,14 @@ const HH<OS> & get_HH(const OSinput& oi, long double mu2)
     return directory.insert(std::make_pair(std::make_pair(oi,mu2), HH<OS>(oi,mu2))).first->second;
 }
 
-const tt & get_tt(const OSinput& oi, long double mu2)
+const tt<OS> & get_tt(const OSinput& oi, long double mu2)
 {
-  static std::map< std::pair<OSinput, long double>, tt > directory;
-  std::map< std::pair<OSinput, long double>, tt >::iterator i = directory.find(std::make_pair(oi,mu2));
+  static std::map< std::pair<OSinput, long double>, tt<OS> > directory;
+  std::map< std::pair<OSinput, long double>, tt<OS> >::iterator i = directory.find(std::make_pair(oi,mu2));
   if (i != directory.end())
     return i->second;
   else
-    return directory.insert(std::make_pair(std::make_pair(oi,mu2), tt(oi,mu2))).first->second;
+    return directory.insert(std::make_pair(std::make_pair(oi,mu2), tt<OS>(oi,mu2))).first->second;
 }
 
 const bb & get_bb(const OSinput& oi, long double mu2)
@@ -60,7 +110,110 @@ const bb & get_bb(const OSinput& oi, long double mu2)
 }
 
 typedef std::vector<std::pair<size_t,size_t> > Vpow;
-void XW(double mb, double mW, double mZ, double mH, double mt, double mu, int nL,int nH) 
+
+
+void MW(long double gp, long double g, long double gs, long double yt, long double lam, long double mu0, long double mu, int L) 
+{
+  long double mu2 = pow(mu,2);
+  MSinput mi = MSinput::fromConsts(mu2, mu0, lam, 0, yt, g, gp);
+  
+  WW<MS> WWm = get_WWbar(mi, mu2);
+
+  long double aEW  = mi.alpha()/4./Pi;
+  long double aQCD = pow(gs/4./Pi,2);
+
+  long double dMW2 = 0;
+  if (L>0) dMW2 += aEW*WWm.x10();
+  if (L>1) dMW2 += aEW*aQCD*WWm.x11() 
+	  	  +aEW*aEW*WWm.x20();
+
+  long double MWpole = mi.mW()*sqrt(1 + dMW2);
+  MLPutReal128(stdlink, MWpole);
+  
+}
+
+void MZ(long double gp, long double g, long double gs, long double yt, long double lam, long double mu0, long double mu, int L) 
+{
+  long double mu2 = pow(mu,2);
+  MSinput mi = MSinput::fromConsts(mu2, mu0, lam, 0, yt, g, gp);
+  
+  ZZ<MS> ZZm = get_ZZbar(mi, mu2);
+
+  long double aEW  = mi.alpha()/4./Pi;
+  long double aQCD = pow(gs/4./Pi,2);
+
+  long double dMZ2 = 0;
+  if (L>0) dMZ2 += aEW*ZZm.x10();
+  if (L>1) dMZ2 += aEW*aQCD*ZZm.x11() 
+	  	  +aEW*aEW*ZZm.x20();
+
+  long double MZpole = mi.mZ()*sqrt(1 + dMZ2);
+  MLPutReal128(stdlink, MZpole);
+  
+}
+
+void MH(long double gp, long double g, long double gs, long double yt, long double lam, long double mu0, long double mu, int L) 
+{
+  long double mu2 = pow(mu,2);
+  MSinput mi = MSinput::fromConsts(mu2, mu0, lam, 0, yt, g, gp);
+  
+  HH<MS> HHm = get_HHbar(mi, mu2);
+
+  long double aEW  = mi.alpha()/4./Pi;
+  long double aQCD = pow(gs/4./Pi,2);
+
+  long double dMH2 = 0;
+  if (L>0) dMH2 += aEW*HHm.x10();
+  if (L>1) dMH2 += aEW*aQCD*HHm.x11() 
+	  	  +aEW*aEW*HHm.x20();
+
+  long double MHpole = mi.mH()*sqrt(1 + dMH2);
+  MLPutReal128(stdlink, MHpole);
+  
+}
+void MT(long double gp, long double g, long double gs, long double yt, long double lam, long double mu0, long double mu, int L) 
+{
+  long double mu2 = pow(mu,2);
+  MSinput mi = MSinput::fromConsts(mu2, mu0, lam, 0, yt, g, gp);
+  
+  tt<MS> ttm = get_ttbar(mi, mu2);
+
+  long double aEW  = mi.alpha()/4./Pi;
+  long double aQCD = pow(gs/4./Pi,2);
+
+  long double dMT2 = 0;
+  if (L>0) dMT2 += aEW*ttm.x10() + aQCD*ttm.x01();
+  if (L>1) dMT2 += aEW*aQCD*ttm.x11() 
+	  	  +aEW*aEW*ttm.x20()
+	          +aQCD*aQCD*ttm.x02();
+  if (L>2) dMT2 +=  aQCD*aQCD*aQCD*ttm.x03();
+
+  long double MTpole = mi.mt()*(1 + dMT2);
+
+  MLPutReal128(stdlink, MTpole);
+  
+}
+void GF(long double gp, long double g, long double gs, long double yt, long double lam, long double mu0, long double mu, int L) 
+{
+  long double mu2 = pow(mu,2);
+  MSinput mi = MSinput::fromConsts(mu2, mu0, lam, 0, yt, g, gp);
+  
+  dr<MS> drm = get_drbar(mi, mu2);
+
+  long double aEW  = mi.alpha()/4./Pi;
+  long double aQCD = pow(gs/4./Pi,2);
+
+  long double dGF = 0;
+  if (L>0) dGF += aEW*drm.dr10();
+  if (L>1) dGF += aEW*aQCD*drm.dr11() 
+	  	  +aEW*aEW*drm.dr20();
+
+  long double GF = (1 + dGF)/sqrt(2.0)/pow(mi.vev(),2);
+  MLPutReal128(stdlink, GF);
+  
+}
+
+void XW(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nL,int nH) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
@@ -86,7 +239,7 @@ void XW(double mb, double mW, double mZ, double mH, double mt, double mu, int nL
       }
 }
 
-void XZ(double mb, double mW, double mZ, double mH, double mt, double mu, int nL,int nH) 
+void XZ(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nL,int nH) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
@@ -112,7 +265,7 @@ void XZ(double mb, double mW, double mZ, double mH, double mt, double mu, int nL
       }
 }
 
-void XH(double mb, double mW, double mZ, double mH, double mt, double mu, int nL,int nH) 
+void XH(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nL,int nH) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
@@ -138,11 +291,11 @@ void XH(double mb, double mW, double mZ, double mH, double mt, double mu, int nL
       }
 }
 
-void Xt(double mb, double mW, double mZ, double mH, double mt, double mu, int nL,int nH) 
+void Xt(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nL,int nH) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
-  tt ttm = get_tt(oi, pow(mu,2));
+  tt<OS> ttm = get_tt(oi, pow(mu,2));
 
   MLPutFunction(stdlink, "List", 6);
 
@@ -164,7 +317,7 @@ void Xt(double mb, double mW, double mZ, double mH, double mt, double mu, int nL
       }
 }
 
-void Xb(double mb, double mW, double mZ, double mH, double mt, double mu, int nL,int nH) 
+void Xb(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nL,int nH) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
@@ -193,7 +346,7 @@ void Xb(double mb, double mW, double mZ, double mH, double mt, double mu, int nL
 
 // Pure QCD corrections
 
-void XbQCD(double mb, double mW, double mZ, double mH, double mt, double mu, int nl, int nh) 
+void XbQCD(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nl, int nh) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
@@ -221,11 +374,11 @@ void XbQCD(double mb, double mW, double mZ, double mH, double mt, double mu, int
 
 }
 
-void XtQCD(double mb, double mW, double mZ, double mH, double mt, double mu, int nl, int nh) 
+void XtQCD(long double mb, long double mW, long double mZ, long double mH, long double mt, long double mu, int nl, int nh) 
 {
   OSinput oi(mb, mW, mZ, mH, mt);
   
-  tt ttm = get_tt(oi, pow(mu,2));
+  tt<OS> ttm = get_tt(oi, pow(mu,2));
 
   MLPutFunction(stdlink, "List", 3);
 
