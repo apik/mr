a559d7a5b36d8315e48b08df31f9c6f125b2a472
Andrey Pikelner <pikelner@theor.jinr.ru>
2015-04-15 19:34:09 +0200

    inverted relation for Mb and mb with non explicit Mt dependence in nf=6 QCd

--- mr/mOS2mMSQCD.cpp
+++ mr/mOS2mMSQCD.cpp
@@ -180,7 +180,7 @@ long double mOS2mMSnm(long double MM, long double x, long double mu2, size_t nl_
   
   if(loops == 1)
     {
-      long double zm1l =
+      std::complex<long double> zm1l =
         (
          - 16./3.
          );
@@ -189,11 +189,11 @@ long double mOS2mMSnm(long double MM, long double x, long double mu2, size_t nl_
         + LmuM * (
                   - 4
                   );
-      return zm1l;
+      return zm1l.real();
     }
   else if(loops == 2)
     {
-      long double zm2l =
+      std::complex<long double> zm2l =
         (
          - 3305./18.
          + 8./3.*Zeta3
@@ -201,79 +201,91 @@ long double mOS2mMSnm(long double MM, long double x, long double mu2, size_t nl_
          - 32./3.*Zeta2*log(2.)
          );
       
-      zm2l +=  nm * (
-                     + 71./9.
-                     + 16./3.*Zeta2
-                     - 16*x*Zeta2
-                     + 8*pow(x,2)
-                     - 16*pow(x,3)*Zeta2
-                     + 16./3.*pow(x,4)*Zeta2
-                     + 16./3.*log(x)*pow(x,2)
-                     - 16./3.*log(x)*log(1 - x)
-                     + 16./3.*log(x)*log(1 - x)*x
-                     + 16./3.*log(x)*log(1 - x)*pow(x,3)
-                     - 16./3.*log(x)*log(1 - x)*pow(x,4)
-                     - 16./3.*log(x)*log(1 + x)
-                     - 16./3.*log(x)*log(1 + x)*x
-                     - 16./3.*log(x)*log(1 + x)*pow(x,3)
-                     );
-  
-      zm2l +=  nm * (
-                     - 16./3.*log(x)*log(1 + x)*pow(x,4)
-                     + 16./3.*pow(log(x),2)*pow(x,4)
-                     - 16./3.*Li2( - x).real()
-                     - 16./3.*Li2( - x).real()*x
-                     - 16./3.*Li2( - x).real()*pow(x,3)
-                     - 16./3.*Li2( - x).real()*pow(x,4)
-                     - 16./3.*Li2(x).real()
-                     + 16./3.*Li2(x).real()*x
-                     + 16./3.*Li2(x).real()*pow(x,3)
-                     - 16./3.*Li2(x).real()*pow(x,4)
-                     );
-  
-      zm2l +=  nl * (
-                     + 71./9.
-                     + 16./3.*Zeta2
-                     );
+      zm2l +=  nm * 
+        (
+         + 71./9.
+         + 16./3.*Zeta2
+         - 16*x*Zeta2
+         + 8*pow(x,2)
+         - 16*pow(x,3)*Zeta2
+         + 16./3.*pow(x,4)*Zeta2
+         + 16./3.*log(x)*pow(x,2)
+         - 16./3.*log(x)*log(1 - x)
+         + 16./3.*log(x)*log(1 - x)*x
+         + 16./3.*log(x)*log(1 - x)*pow(x,3)
+         - 16./3.*log(x)*log(1 - x)*pow(x,4)
+         - 16./3.*log(x)*log(1 + x)
+         - 16./3.*log(x)*log(1 + x)*x
+         - 16./3.*log(x)*log(1 + x)*pow(x,3)
+         );
       
-      zm2l +=  nh * (
-                     + 143./9.
-                     - 32./3.*Zeta2
-                     );
+      zm2l +=  nm * 
+        (
+         - 16./3.*log(x)*log(1 + x)*pow(x,4)
+         + 16./3.*pow(log(x),2)*pow(x,4)
+         - 16./3.*Li2( - x)
+         - 16./3.*Li2( - x)*x
+         - 16./3.*Li2( - x)*pow(x,3)
+         - 16./3.*Li2( - x)*pow(x,4)
+         - 16./3.*Li2(x)
+         + 16./3.*Li2(x)*x
+         + 16./3.*Li2(x)*pow(x,3)
+         - 16./3.*Li2(x)*pow(x,4)
+         );
       
-      zm2l +=  LmuM * (
-                       - 314./3.
-                       );
-  
-      zm2l +=  LmuM*nm * (
-                          + 52./9.
-                          );
-  
-      zm2l +=  LmuM*nl * (
-                          + 52./9.
-                          );
-  
-      zm2l +=  LmuM*nh * (
-                          + 52./9.
-                          );
-  
-      zm2l +=  pow(LmuM,2) * (
-                              - 14
-                              );
+      zm2l +=  nl * 
+        (
+         + 71./9.
+         + 16./3.*Zeta2
+         );
       
-      zm2l +=  pow(LmuM,2)*nm * (
-                                 + 4./3.
-                                 );
+      zm2l +=  nh * 
+        (
+         + 143./9.
+         - 32./3.*Zeta2
+         );
       
-      zm2l +=  pow(LmuM,2)*nl * (
-                                 + 4./3.
-                                 );
+      zm2l +=  LmuM * 
+        (
+         - 314./3.
+         );
       
-      zm2l +=  pow(LmuM,2)*nh * (
-                                 + 4./3.
-                                 );
+      zm2l +=  LmuM*nm * 
+        (
+         + 52./9.
+         );
+      
+      zm2l +=  LmuM*nl * 
+        (
+         + 52./9.
+         );
+      
+      zm2l +=  LmuM*nh * 
+        (
+         + 52./9.
+         );
+      
+      zm2l +=  pow(LmuM,2) * 
+        (
+         - 14
+         );
+      
+      zm2l +=  pow(LmuM,2)*nm * 
+        (
+         + 4./3.
+         );
+      
+      zm2l +=  pow(LmuM,2)*nl * 
+        (
+         + 4./3.
+         );
+      
+      zm2l +=  pow(LmuM,2)*nh * 
+        (
+         + 4./3.
+         );
 
-      return zm2l;
+      return zm2l.real();
     }
   else
     return 0;
@@ -513,6 +525,98 @@ long double mMS2mOS(long double mm, long double mu2, size_t nl, size_t nh, size_
   else 
     return 0;
 }
+
+// Botom mass in nf=6 scheme with explicit dependence on top quark
+// mass
+//
+//    M - Pole mass of b-quark
+//    Lmumb=Log(mu^2/mb^2)
+//
+//    x=m_t/m
+//
+// 
+long double mMS2mOSnm(long double mm, long double x, long double mu2, size_t nl_,size_t nm_, size_t nh_, size_t loops)
+{
+  double nl = double(nl_), nm = double (nm_), nh = double (nh_);
+  
+  long double Lmumb = log(mu2/mm);
+  long double mb = sqrt(mm);
+  long double mt = x*mb;
+  
+  if(loops == 1)
+    {
+      std::complex<long double> zm1l = 
+        (
+         + 16./3.
+         + 4*Lmumb
+         );
+      return zm1l.real();
+    }
+  if(loops == 2)
+    {
+      std::complex<long double> zm2l =  
+        (
+         + 3049./18.
+         - 8./3.*Zeta3
+         + 64./3.*Zeta2
+         + 346./3.*Lmumb
+         + 30*pow(Lmumb,2)
+         + 32./3.*log(2)*Zeta2
+         );
+      
+      zm2l += nm *
+         (
+          - 71./9.
+          - 16./3.*pow(mb,-4)*pow(mt,4)*Zeta2
+          + 16*pow(mb,-3)*pow(mt,3)*Zeta2
+          - 8*pow(mb,-2)*pow(mt,2)
+          - 16./3.*Zeta2
+          - 52./9.*Lmumb
+          - 4./3.*pow(Lmumb,2)
+          + 16*x*Zeta2
+          - 16./3.*log(x)*pow(mb,-2)*pow(mt,2)
+          + 16./3.*log(x)*log(1 - x)
+          + 16./3.*log(x)*log(1 - x)*pow(mb,-4)*pow(mt,4)
+          - 16./3.*log(x)*log(1 - x)*pow(mb,-3)*pow(mt,3)
+          - 16./3.*log(x)*log(1 - x)*x
+          + 16./3.*log(x)*log(1 + x)
+          );
+      
+      zm2l += nm * 
+        (
+         + 16./3.*log(x)*log(1 + x)*pow(mb,-4)*pow(mt,4)
+         + 16./3.*log(x)*log(1 + x)*pow(mb,-3)*pow(mt,3)
+         + 16./3.*log(x)*log(1 + x)*x
+         - 16./3.*pow(log(x),2)*pow(mb,-4)*pow(mt,4)
+         + 16./3.*Li2( - x)
+         + 16./3.*Li2( - x)*pow(mb,-4)*pow(mt,4)
+         + 16./3.*Li2( - x)*pow(mb,-3)*pow(mt,3)
+         + 16./3.*Li2( - x)*x
+         + 16./3.*Li2(x)
+         + 16./3.*Li2(x)*pow(mb,-4)*pow(mt,4)
+         - 16./3.*Li2(x)*pow(mb,-3)*pow(mt,3)
+         - 16./3.*Li2(x)*x
+         );
+      
+      zm2l +=  nl * 
+        (
+         - 71./9.
+         - 16./3.*Zeta2
+         - 52./9.*Lmumb
+         - 4./3.*pow(Lmumb,2)
+         );
+      
+      zm2l +=  nh * 
+        (
+         - 143./9.
+         + 32./3.*Zeta2
+         - 52./9.*Lmumb
+         - 4./3.*pow(Lmumb,2)
+         );
+      return zm2l.real();
+    }
+}
+
 
 // bottom
 long double bb<MS>::x02(size_t nL, size_t nH, size_t boson)
