b577dde39f40e1feba688bd45d8c50e56a6f4a39
Andrey Pikelner <pikelner@theor.jinr.ru>
2015-08-13 19:18:18 +0200

    operator for complex numbers now available only inside namespace

--- mr/operators.hpp
+++ mr/operators.hpp
@@ -21,59 +21,61 @@
 #ifndef __OPERATORS_HPP__
 #define __OPERATORS_HPP__
 
-template <typename T, typename U>
-inline std::complex<T> operator*(std::complex<T> lhs, const U& rhs)
+namespace mr
 {
-  return lhs *= rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator*(std::complex<T> lhs, const U& rhs)
+  {
+    return lhs *= rhs;
+  }
 
-template <typename T, typename U>
-inline std::complex<T> operator*(const U& rhs, std::complex<T> lhs)
-{
-  return lhs *= rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator*(const U& rhs, std::complex<T> lhs)
+  {
+    return lhs *= rhs;
+  }
 
-template <typename T, typename U>
-inline std::complex<T> operator/(std::complex<T> lhs, const U& rhs)
-{
-  return lhs /= rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator/(std::complex<T> lhs, const U& rhs)
+  {
+    return lhs /= rhs;
+  }
 
 
-template <typename T, typename U>
-inline std::complex<T> operator/(const U& lhs, std::complex<T> rhs)
-{
-  return std::complex<T>(lhs) /= rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator/(const U& lhs, std::complex<T> rhs)
+  {
+    return std::complex<T>(lhs) /= rhs;
+  }
 
-template <typename T, typename U>
-inline std::complex<T> operator+(std::complex<T> lhs,const U& rhs)
-{
-  return lhs += rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator+(std::complex<T> lhs,const U& rhs)
+  {
+    return lhs += rhs;
+  }
 
-template <typename T, typename U>
-inline std::complex<T> operator+(const U& rhs, std::complex<T> lhs)
-{
-  return lhs += rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator+(const U& rhs, std::complex<T> lhs)
+  {
+    return lhs += rhs;
+  }
 
-template <typename T, typename U>
-inline std::complex<T> operator-(std::complex<T> lhs, const U& rhs)
-{
-  return lhs -= rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator-(std::complex<T> lhs, const U& rhs)
+  {
+    return lhs -= rhs;
+  }
 
-template <typename T, typename U>
-inline std::complex<T> operator-(const U& lhs, std::complex<T> rhs)
-{
-  return std::complex<T>(lhs) -= rhs;
-}
+  template <typename T, typename U>
+  inline std::complex<T> operator-(const U& lhs, std::complex<T> rhs)
+  {
+    return std::complex<T>(lhs) -= rhs;
+  }
 
-template <typename T>
-inline std::complex<T> Re(std::complex<T> z)
-{
-  return std::complex<T>(z.real(),0);
-}
-
+  template <typename T>
+  inline std::complex<T> Re(std::complex<T> z)
+  {
+    return std::complex<T>(z.real(),0);
+  }
+} // namespace mr
 #endif  // __OPERATORS_HPP__
