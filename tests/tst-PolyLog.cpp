fc26c9ba4b6f04515ec045eafd3ed80da22fafa4
Andrey Pikelner <pikelner@theor.jinr.ru>
2019-08-16 14:47:26 +0300

    Added missing files

---
+++ tests/tst-PolyLog.cpp
@@ -0,0 +1,55 @@
+#include "catch.hpp"
+#include "mr.hpp"
+
+using namespace mr;
+
+///////////////////////////////////////////////////////////////////////////////
+TEST_CASE
+(
+ "Check DiLog and TriLog evaluation implemented in TSIL",
+ "[PolyLog]"
+ )
+{
+  
+  
+  SECTION( "Real arguments" )
+    {
+
+      std::complex<long double> li2 = mr::Li2( std::complex<long double>(1,0) );
+      REQUIRE( li2.real() == Approx( 1.64493 ) );
+      REQUIRE( li2.imag() == Approx( 0 ) );
+
+      std::complex<long double> li3 = mr::Li3( std::complex<long double>(1,0) );
+      REQUIRE( li3.real() == Approx( 1.20206 ) );
+      REQUIRE( li3.imag() == Approx( 0 ) );
+
+    }
+
+  SECTION( "Imaginary arguments" )
+    {
+
+      std::complex<long double> li2 = mr::Li2( std::complex<long double>(0,1) );
+      REQUIRE( li2.real() == Approx( -0.205617 ) );
+      REQUIRE( li2.imag() == Approx( 0.915966 ) );
+
+      std::complex<long double> li3 = mr::Li3( std::complex<long double>(0,1) );
+      REQUIRE( li3.real() == Approx( -0.112693 ) );
+      REQUIRE( li3.imag() == Approx( 0.968946 ) );
+
+    }
+
+  SECTION( "Complex arguments" )
+    {
+
+      std::complex<long double> li2 = mr::Li2( std::complex<long double>(1,1) );
+      REQUIRE( li2.real() == Approx( 0.61685 ) );
+      REQUIRE( li2.imag() == Approx( 1.46036 ) );
+
+      std::complex<long double> li3 = mr::Li3( std::complex<long double>(1,1) );
+      REQUIRE( li3.real() == Approx( 0.871159 ) );
+      REQUIRE( li3.imag() == Approx( 1.26708 ) );
+
+    }
+
+
+}
